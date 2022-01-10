41f68d15e94f1392c5595f03e41c8b999476d458
cht <3237126351@qq.com>
2022-01-10 19:23:02 +0800

    優化

--- Gisture.cs
+++ Gisture.cs
@@ -27,7 +27,7 @@ namespace Gisture
         [LoadColumn(14), ColumnName("Label")]
         public int Kind { get; set; }
     }
-    class DataPoint
+    internal class  DataPoint
     {
         public bool Label { get; set; }
 
