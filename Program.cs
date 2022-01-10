d050e0f0d84f54c1453d5215b12973c277e25862
cht <3237126351@qq.com>
2022-01-10 19:27:16 +0800

    two test

--- Program.cs
+++ Program.cs
@@ -15,7 +15,17 @@
 				ThumbIndex = -59.3171f,
 				IndexMiddle = 1.09537f,
 			};
+			var forpredict2 = new DataPoint {
+				ThumbAngle=62.4204f,
+				IndexAngle= -8.53424f,
+				MiddleAngle= 1.08902f,
+				RingAngle=	157.382f,
+				PinkyAngle= 110.563f,
+				ThumbIndex =  60.698f,
+				IndexMiddle =  5.03313f
+			};
 			Console.WriteLine(trainer.Predict(forpredict));
+			Console.WriteLine(trainer.Predict(forpredict2));
 
         }
     }
